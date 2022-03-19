02f2b6964f997c11eb5d9082fceab5f09e723a76
gulayfirat <guleturcan@gmail.com>
2022-03-19 20:43:42 +0300

    language manager and app_constants created

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,3 @@
+class AppConstants {
+  static const langAssetPath = "assets/lang";
+}
---
+++ lib/core/init/lang/locale_keys.g.dart
@@ -0,0 +1,16 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const appBar_title = 'appBar.title';
+  static const appBar = 'appBar';
+  static const login_signInButton = 'login.signInButton';
+  static const login_emailText = 'login.emailText';
+  static const login_passwordText = 'login.passwordText';
+  static const login = 'login';
+  static const register_signUpButton = 'register.signUpButton';
+  static const register_emailText = 'register.emailText';
+  static const register_passwordText = 'register.passwordText';
+  static const register_username = 'register.username';
+  static const register = 'register';
+
+}
