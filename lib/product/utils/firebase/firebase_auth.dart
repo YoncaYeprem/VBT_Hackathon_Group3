68fdf1ef0769a01991d05d74c3a28a37135d0769
Rüçhan Adıgüzel <jceadsa@gmail.com>
2022-03-19 18:03:40 +0300

    firebase auth added

---
+++ lib/product/utils/firebase/firebase_auth.dart
@@ -0,0 +1,51 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import '../../../feature/search_view/search_view.dart';
+
+class Authentication {
+  Future<User?> eMailSignIn(
+      {required String eMail,
+      required String password,
+      required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+          email: eMail, password: password);
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-found") {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text("No user found")));
+      }
+    }
+    return user;
+  }
+
+  Future<void> signUp(
+      String email, String password, BuildContext context) async {
+    try {
+      UserCredential credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "email-already-in-use") {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text("Mail already using")));
+      }
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<FirebaseApp> initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+}
