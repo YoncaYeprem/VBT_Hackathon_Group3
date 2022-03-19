3475af530b977525d0a51153e1d6c6b454701a26
gulayfirat <guleturcan@gmail.com>
2022-03-19 20:44:59 +0300

    easy_localization configuration done

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:vbt_hackathon_group3/core/init/lang/locale_keys.g.dart';
+import 'core/constants/app_constants.dart';
+import 'core/constants/language_manager.dart';
 import 'core/init/theme/app_theme.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await EasyLocalization.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(EasyLocalization(
+      child: const MyApp(),
+      supportedLocales: LanguageManager.instance.supportedLanguages,
+      startLocale: LanguageManager.instance.trLocale,
+      path: AppConstants.langAssetPath));
 }
 
 class MyApp extends StatelessWidget {
@@ -19,13 +30,16 @@ class MyApp extends StatelessWidget {
       title: 'Material App',
       debugShowCheckedModeBanner: false,
       theme: ThemeManager.createThemeData(AppThemeLight()),
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: Text(LocaleKeys.appBar_title.tr()),
         ),
         body: Center(
           child: Container(
-            child: Text('Hello World'),
+            child: const Text('Hello World'),
           ),
         ),
       ),
