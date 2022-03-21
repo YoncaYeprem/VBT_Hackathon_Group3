43168879c82c7c863cf2e02ea2b8a039d70364e2
Yonca Yeprem <yonn2798@gmail.com>
2022-03-21 16:41:48 +0300

    register cubit state management created and register operations done

--- lib/feature/authentication/register/viewmodel/cubit/register_state.dart
+++ lib/feature/authentication/register/viewmodel/cubit/register_state.dart
@@ -4,3 +4,19 @@ part of 'register_cubit.dart';
 abstract class RegisterState {}
 
 class RegisterInitial extends RegisterState {}
+
+class RegisterSelectImage extends RegisterState {
+  RegisterSelectImage();
+}
+
+class RegisterLoading extends RegisterState {
+  RegisterLoading();
+}
+
+class RegisterComplete extends RegisterState {
+  RegisterComplete();
+}
+
+class RegisterFailed extends RegisterState {
+  RegisterFailed();
+}
