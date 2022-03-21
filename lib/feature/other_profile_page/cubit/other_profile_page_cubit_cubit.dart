169138fd9857c30c1168ae8cf78a1143a0fb1607
Rüçhan Adıgüzel <jceadsa@gmail.com>
2022-03-21 16:26:00 +0300

    page's skeleton made

---
+++ lib/feature/other_profile_page/cubit/other_profile_page_cubit_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'other_profile_page_cubit_state.dart';
+
+class OtherProfilePageCubitCubit extends Cubit<OtherProfilePageCubitState> {
+  OtherProfilePageCubitCubit() : super(OtherProfilePageCubitInitial());
+}
---
+++ lib/feature/other_profile_page/cubit/other_profile_page_cubit_state.dart
@@ -0,0 +1,6 @@
+part of 'other_profile_page_cubit_cubit.dart';
+
+@immutable
+abstract class OtherProfilePageCubitState {}
+
+class OtherProfilePageCubitInitial extends OtherProfilePageCubitState {}
