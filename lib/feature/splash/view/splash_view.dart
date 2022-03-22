9941c8565a20f8169849feba9adf426def6762f7
gulayfirat <guleturcan@gmail.com>
2022-03-22 14:24:20 +0300

    splash page created

--- lib/feature/splash/view/splash_view.dart
+++ lib/feature/splash/view/splash_view.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+
+import '../../authentication/login/view/login_view.dart';
 
 class SplashView extends StatelessWidget {
-  const SplashView({ Key? key }) : super(key: key);
-
+  const SplashView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return AnimatedSplashScreen(
+      splashIconSize: 350,
+      duration: 3500,
+      splash: 'assets/gifs/splash.gif',
+      nextScreen: LoginView(),
+      splashTransition: SplashTransition.sizeTransition,
     );
   }
 }
