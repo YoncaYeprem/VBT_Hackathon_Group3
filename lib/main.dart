8f2b1f7ac4e0c50b55219888639d3e37d51531cf
Yonca Yeprem <yonn2798@gmail.com>
2022-03-22 14:22:04 +0300

    hive initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'feature/authentication/login/view/login_view.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
 import 'core/constants/app_constants.dart';
 import 'core/constants/language_manager.dart';
 import 'core/init/theme/app_theme.dart';
-import 'feature/authentication/register/view/register_view.dart';
+import 'feature/addBook/model/product_model.dart';
 import 'feature/bookDetail/view/book_detail_view.dart';
+import 'feature/fakeData/book_data.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -15,6 +18,10 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(BookModelAdapter());
+  await Hive.openBox<BookModel>('books');
   runApp(EasyLocalization(
       child: MyApp(),
       supportedLocales: LanguageManager.instance.supportedLocales,
@@ -33,7 +40,7 @@ class MyApp extends StatelessWidget {
       title: 'Hackathon App',
       debugShowCheckedModeBanner: false,
       theme: ThemeManager.createThemeData(AppThemeLight()),
-      home: BookDetailView(),
+      home: BookDetailView(book:book1),
     );
   }
 }
