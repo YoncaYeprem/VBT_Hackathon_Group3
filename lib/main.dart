b4dd3592292f152b471b929af1e1f0201ce020f7
Yonca Yeprem <yonn2798@gmail.com>
2022-03-23 21:50:44 +0300

    path changed

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-
 import 'core/constants/app_constants.dart';
 import 'core/constants/language_manager.dart';
 import 'core/init/theme/app_theme.dart';
-import 'feature/addBook/model/product_model.dart';
+import 'feature/addBook/model/book_model.dart';
 import 'feature/bookDetail/view/book_detail_view.dart';
 import 'feature/fakeData/book_data.dart';
+import 'feature/myFavoritesPage/view/my_favorites_view.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
       title: 'Hackathon App',
       debugShowCheckedModeBanner: false,
       theme: ThemeManager.createThemeData(AppThemeLight()),
-      home: BookDetailView(book:book1),
+      home: BookDetailView(book : book1),
     );
   }
 }
