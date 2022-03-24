50799eec160de06d037219093523c9833033304e
onur0481 <63808974+onur0481@users.noreply.github.com>
2022-03-24 21:45:24 +0300

    update github

--- lib/product/widget/custom_profile_detail_list.dart
+++ lib/product/widget/custom_profile_detail_list.dart
@@ -23,7 +23,7 @@ class ProfileInformation extends StatelessWidget {
           GestureDetector(
             child: const Card(
               child: ListTile(
-                title: Text("Kitaplarım"),
+                title: Text("Kitaplarim"),
                 trailing: Icon(Icons.arrow_forward_ios_rounded),
               ),
             ),
